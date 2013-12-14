1babf94d19bda92fa5f4a79526bc2e4da6ed5e2c
juho H <juhoth@ovi.com>
2013-12-14 21:03:31 +0200

    some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+stadin = Brewery.create!(name: "Stadin Panimo", year: 1997)
+weihen = Brewery.create!(name: "Weihenstephan", year: 1042)
+
