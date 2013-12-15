62d572bc5272ab5617b09daeceb12f87f1674319
juho H <juhoth@ovi.com>
2013-12-16 00:27:33 +0200

    brewery seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
+Brewery.destroy_all
 stadin = Brewery.create!(name: "Stadin Panimo", year: 1997)
 weihen = Brewery.create!(name: "Weihenstephan", year: 1042)
 
