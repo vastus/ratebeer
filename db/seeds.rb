c2f6696a5ddf5a367368b69c853fb7911d34157b
juho H <juhoth@ovi.com>
2013-12-16 02:20:53 +0200

    beer seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
 Brewery.destroy_all
+koff = Brewery.create!(name: "Koff", year: 1897)
 stadin = Brewery.create!(name: "Stadin Panimo", year: 1997)
 weihen = Brewery.create!(name: "Weihenstephan", year: 1042)
+
+Beer.destroy_all
+iso3 = Beer.create!(name: "Iso 3", style: "Lager", brewery_id: koff.id)
+karhu = Beer.create!(name: "Karhu", style: "Lager", brewery_id: koff.id)
+
 
