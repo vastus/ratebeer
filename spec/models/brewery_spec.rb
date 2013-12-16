fb2fc563f97d5d5aeacf1f12897daf3999dc4467
juho H <juhoth@ovi.com>
2013-12-16 02:15:32 +0200

    teht4 spec

--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Brewery do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Tehtävä 4" do
+    it "updates updated_at when updating attributes" do
+      Brewery.create!(name: "Kumpulan panimo", year: 2012)
+      kumpula = Brewery.find_by_name("Kumpulan panimo")
+      kumpula.update_attributes(year: 2013)
+      expect(kumpula.updated_at).to_not eq kumpula.created_at
+    end
+  end
 end
