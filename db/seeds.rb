00197ce5a4431b089de577dfd1bb43e4198d0179
juho H <juhoth@ovi.com>
2014-01-24 15:02:20 +0200

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,26 @@ Brewery.destroy_all
 koff = Brewery.create!(:name => "Koff", :year => 1897)
 malmgard = Brewery.create!(:name => "Malmgard", :year => 2001)
 weihen = Brewery.create!(:name => "Weihenstephaner", :year => 1042)
+brewdog = Brewery.create!(name: "BrewDog", year: 2007)
 
 Beer.destroy_all
-koff.beers.create!(:name => "Iso 3", :style => "Lager")
-koff.beers.create!(:name => "Karhu", :style => "Lager")
-koff.beers.create!(:name => "Tuplahumala", :style => "Lager")
-malmgard.beers.create!(:name => "Huvila Pale Ale", :style => "Pale Ale")
-malmgard.beers.create!(:name => "X Porter", :style => "Porter")
-weihen.beers.create!(:name => "Hefezeizen", :style => "Weizen")
-weihen.beers.create!(:name => "Helles", :style => "Lager")
+iso3 = koff.beers.create!(:name => "Iso 3", :style => "Lager")
+karhu = koff.beers.create!(:name => "Karhu", :style => "Lager")
+tupla = koff.beers.create!(:name => "Tuplahumala", :style => "Lager")
+huvila = malmgard.beers.create!(:name => "Huvila Pale Ale", :style => "Pale Ale")
+xporter = malmgard.beers.create!(:name => "X Porter", :style => "Porter")
+hefe = weihen.beers.create!(:name => "Hefezeizen", :style => "Weizen")
+helles = weihen.beers.create!(:name => "Helles", :style => "Lager")
+punk = brewdog.beers.create!(name: "Punk IPA", style: "IPA")
+nanny = brewdog.beers.create!(name: "Nanny State", style: "lowalcohol")
+
+Rating.destroy_all
+iso3.ratings.create!(score: 10)
+iso3.ratings.create!(score: 21)
+iso3.ratings.create!(score: 17)
+punk.ratings.create!(score: 39)
+punk.ratings.create!(score: 25)
+punk.ratings.create!(score: 45)
+nanny.ratings.create!(score: 14)
+nanny.ratings.create!(score: 21)
 
