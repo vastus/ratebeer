7366451a08be5fccc307b45db38416bd3bc21b68
juho H <juhoth@ovi.com>
2014-01-26 22:59:25 +0200

    make use of the RatingAverage mixin

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,12 +1,11 @@
 class Beer < ActiveRecord::Base
+  # mixins
+  include RatingAverage
+
   # associations
   belongs_to(:brewery)
 
   has_many(:ratings, dependent: :destroy)
-
-  def average_rating
-    ratings.average(:score).to_f.round(2)
-  end
 
   def to_s
     "#{brewery.name}'s #{name}"
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,10 +1,9 @@
 class Brewery < ActiveRecord::Base
+  # mixins
+  include RatingAverage
+
   # associations
   has_many(:beers, dependent: :destroy)
   has_many(:ratings, through: :beers)
-
-  def average_rating
-    ratings.average(:score).to_f.round(2)
-  end
 end
 
