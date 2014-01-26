e8136b86f14be679d8a189945fb6dc0bf5821bc5
juho H <juhoth@ovi.com>
2014-01-26 22:23:13 +0200

    destroy route for ratings

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Ratebeer::Application.routes.draw do
 
   resources :breweries
 
-  resources(:ratings, only: [:index, :new, :create])
+  resources(:ratings, only: [:index, :new, :create, :destroy])
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
