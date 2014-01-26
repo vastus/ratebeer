2e35606de0958346860da58f0a93f30f3a665df7
juho H <juhoth@ovi.com>
2014-01-26 22:49:24 +0200

    brewery knows its avg rating

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,5 +2,9 @@ class Brewery < ActiveRecord::Base
   # associations
   has_many(:beers, dependent: :destroy)
   has_many(:ratings, through: :beers)
+
+  def average_rating
+    ratings.average(:score).to_f.round(2)
+  end
 end
 
