943af184b7ac9f850954b93369ed1b22b54c9b7e
juho H <juhoth@ovi.com>
2014-01-26 22:30:08 +0200

    rating destroy action

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -12,5 +12,11 @@ class RatingsController < ApplicationController
     Rating.create!(params.require(:rating).permit(:beer_id, :score))
     redirect_to(ratings_path)
   end
+
+  def destroy
+    rating = Rating.find(params[:id])
+    rating.delete
+    redirect_to(ratings_path)
+  end
 end
 
