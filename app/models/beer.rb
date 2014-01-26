a38cd66aac9cae5588ccf94ae609e8e8ba5e67ce
juho H <juhoth@ovi.com>
2014-01-26 22:37:19 +0200

    destroy ratings w/ beer

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,7 +2,7 @@ class Beer < ActiveRecord::Base
   # associations
   belongs_to(:brewery)
 
-  has_many(:ratings)
+  has_many(:ratings, dependent: :destroy)
 
   def average_rating
     ratings.average(:score).to_f.round(2)
