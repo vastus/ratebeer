721cd3e95633f00ee91883b3e9ed49429b0398c4
Juho H <juhoth@ovi.com>
2014-01-31 18:36:30 +0200

    must have name and 1042 <= year <= 2014

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -5,5 +5,14 @@ class Brewery < ActiveRecord::Base
   # associations
   has_many(:beers, dependent: :destroy)
   has_many(:ratings, through: :beers)
+
+  # validations
+  validates(:name,
+    presence: true)
+  validates(:year,
+    numericality: {
+      greater_than_or_equal_to: 1042,
+      less_than_or_equal_to: 2014,
+      only_integer: true})
 end
 
