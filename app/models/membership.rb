f42f9a16be3a76adb9583b918007c33d0ac8cce0
Juho H <juhoth@ovi.com>
2014-02-02 22:10:24 +0200

    user can join beer club only once

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,5 +2,17 @@ class Membership < ActiveRecord::Base
   # associations
   belongs_to(:user)
   belongs_to(:beer_club)
+
+  # callbacks
+  before_save(:check_club_members)
+
+  private
+  def check_club_members
+    beer_club = BeerClub.find(beer_club_id)
+    if beer_club && beer_club.user_ids.include?(user_id)
+      errors.add(:user_id, 'already in this club')
+      return false
+    end
+  end
 end
 
