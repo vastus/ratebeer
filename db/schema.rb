b7d68c4283b444666e4d57dd9fedc4bdce63fda1
Juho H <juhoth@ovi.com>
2014-02-02 20:36:22 +0200

    gen beerclub model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140202165721) do
+ActiveRecord::Schema.define(version: 20140202183538) do
+
+  create_table "beer_clubs", force: true do |t|
+    t.string   "name"
+    t.integer  "founded"
+    t.string   "city"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "beers", force: true do |t|
     t.string   "name"
