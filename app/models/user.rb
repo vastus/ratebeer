1751afa6a8421b17b501f3ab944471a411a525d6
Juho H <juhoth@ovi.com>
2014-02-02 19:41:37 +0200

    valid passwd has one no and one capital char

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,11 @@ class User < ActiveRecord::Base
   # validations
   validates(:username,
     uniqueness: true,
-    length: {minimum: 3, maximum: 15})
+    length: { minimum: 3, maximum: 15 })
+  validates(:password,
+    length: { minimum: 4 },
+    format: {
+      with: /.*([A-Z0-9])+.*([A-Z0-9])+/,
+      message: "must contain at least one number and one capital letter" })
 end
 
