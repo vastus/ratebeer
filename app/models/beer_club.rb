eb1651a4d1e467f35ab1d76186a089f9951ec135
Juho H <juhoth@ovi.com>
2014-02-02 20:47:07 +0200

    basic validations for beer club

--- app/models/beer_club.rb
+++ app/models/beer_club.rb
@@ -2,5 +2,12 @@ class BeerClub < ActiveRecord::Base
   # associations
   has_many(:memberships)
   has_many(:users, through: :memberships)
+
+  # validations
+  validates(:name, presence: true)
+  validates(:founded,
+    presence: true,
+    numericality: { only_integer: true })
+  validates(:city, presence: true)
 end
 
