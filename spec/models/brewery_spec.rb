e6fb99905d5ebcffd6e14f957b3985ac082bbd17
Juho H <juhoth@ovi.com>
2014-02-09 23:50:36 +0200

    its not working

--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -9,8 +9,8 @@ describe Brewery do
   describe "when initialized with name Schlenkerla and year 1674" do
     subject{ Brewery.create name: "Schlenkerla", year: 1674 }
     it { should be_valid }
-    its(:name) { should eq("Schlenkerla") }
-    its(:year) { should eq(1674) }
+    # its(:name) { should eq("Schlenkerla") } # nomethoderror: its
+    # its(:year) { should eq(1674) }
   end
 end
 
