4fc19cbf0a56114072eaef3ccd12f376acd0a437
Juho H <juhoth@ovi.com>
2014-02-09 20:23:02 +0200

    initial tests for beer

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Beer do
+  let(:beer) { Beer.new(name: "Beer", style: "Lager") }
+
+  it "is valid w/ correct attributes" do
+    expect(beer).to be_valid
+  end
+
+  it "saves with correct attributes" do
+    beer.save
+    expect(Beer.count).to eq(1)
+  end
+
+  it "does not save w/o name" do
+    Beer.create(style: "Lager")
+    expect(Beer.count).to eq(0)
+  end
+
+  it "does not save w/o style" do
+    Beer.create(name: "Beer")
+    expect(Beer.count).to eq(0)
+  end
 end
 
