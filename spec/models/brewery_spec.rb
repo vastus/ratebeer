d520882f87c9f6c31e71fd6a627a64be8c6dd14a
Juho H <juhoth@ovi.com>
2014-02-09 23:25:13 +0200

    initial brewery tests

--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Brewery do
+  it "without a name is not valid" do
+    brewery = Brewery.create(year: 1674)
+    brewery.should_not be_valid
+  end
+
+  describe "when initialized with name Schlenkerla and year 1674" do
+    subject{ Brewery.create name: "Schlenkerla", year: 1674 }
+    it { should be_valid }
+    its(:name) { should eq("Schlenkerla") }
+    its(:year) { should eq(1674) }
+  end
 end
 
